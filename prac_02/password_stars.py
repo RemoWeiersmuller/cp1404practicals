64143d1f9b7eae0089ec2c5154e9dd453396aeb6
RemoWeiersmuller <remo.weiersmueller@my.jcu.edu.au>
2023-09-20 00:45:15 +0200

    Refactor password check program to use functions

--- prac_02/password_stars.py
+++ prac_02/password_stars.py
@@ -1,9 +1,27 @@
-password = input("your password:")
-minimum_length = 0
+"""Check the password"""
 
-while int(len(password)) <= minimum_length:
-    print("invalid password")
+
+def main():
+    """get valid password and display stars in the pw length"""
+    minimum_length = 0
+
+    password = get_password(minimum_length)
+
+    print_stars(password)
+
+
+def print_stars(password):
+    """display stars in the length of the password given"""
+    print('*' * len(password))
+
+
+def get_password(minimum_length):
+    """get a valid password"""
     password = input("your password:")
+    while int(len(password)) <= minimum_length:
+        print("invalid password")
+        password = input("your password:")
+    return password
 
-print('*' * len(password))
 
+main()
