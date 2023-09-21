d666fa6ab69b9a2a999bbd4b56cedacc45edb42a
RemoWeiersmuller <remo.weiersmueller@my.jcu.edu.au>
2023-09-21 03:12:29 +0200

    add score_menu.py to Repository

---
+++ prac_02/score_menu.py
@@ -0,0 +1,45 @@
+"""Score menu for practical 2"""
+
+from score import determine_grade # TODO how to import without immediatly running function?
+
+
+def main():
+    print("(G)et valid score\n(P)rint result\n(S)how stars\n(Q)uit")
+    choice = input("choice: ").upper()
+
+    while choice != 'Q':
+        if choice == 'G':
+            score = get_valid_score()
+        elif choice == 'P':
+            if score != '':
+                grade = determine_grade(score)
+                print(grade)
+            else:
+                score = get_valid_score()
+                grade = determine_grade(score)
+                print(grade)
+        elif choice == 'S':
+            if score != '':
+                print('*' * score)
+            else:
+                score = get_valid_score()
+                print('*' * score)
+        else:
+            print('value is invalid')
+
+        print("(G)et valid score\n(P)rint result\n(S)how stars\n(Q)uit")
+        choice = input("choice: ").upper()
+
+    print('farewell')
+
+
+def get_valid_score():
+    """get a valid score from user"""
+    score = int(input("Score: "))
+    while score < 0 or score > 100:
+        print("Invalid score!")
+        score = int(input("Score: "))
+    return score
+
+
+main()
