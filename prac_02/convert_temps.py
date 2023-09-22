80a0d7bc8de97d1241daa334f8fd630f601640e8
jc986115 <remo.weiersmueller@my.jcu.edu.au>
2023-09-22 11:43:58 +1000

    add read part not finished

--- prac_02/convert_temps.py
+++ prac_02/convert_temps.py
@@ -1,12 +1,31 @@
 """create temps_input.txt with random temperature in fahrenheit and converts it to celsius in an onther file"""
 
 from random import uniform
+from temperatures import convert_fahrenheit_to_celsius
+from temperatures import convert_celsius_to_fahrenheit
 
 
 def main():
+    generate_fahrenheit_file()
+
+    get_values_from_file()
+
+
+
+
+def get_values_from_file():
+    file = open('temps_input.txt', 'r')
+    for i in range(15):
+        value = file.readline()
+
+
+
+def generate_fahrenheit_file():
+    """generates file with 15 random floats"""
     with open('temps_input.txt', 'w') as f:
         for i in range(15):
-            f.write(str(uniform(-200, 200)))
+            temperature = uniform(-200, 200)
+            f.write(f"{temperature}\n")
 
 
 main()
