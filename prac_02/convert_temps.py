48a48b239128007003fee9b8277af9f8423db04a
jc986115 <remo.weiersmueller@my.jcu.edu.au>
2023-09-22 11:20:44 +1000

    add for loop

--- prac_02/convert_temps.py
+++ prac_02/convert_temps.py
@@ -1,10 +1,12 @@
 """create temps_input.txt with random temperature in fahrenheit and converts it to celsius in an onther file"""
 
 from random import uniform
+
+
 def main():
-
     with open('temps_input.txt', 'w') as f:
-        f.write('Create a new text file!')
+        for i in range(15):
+            f.write(str(uniform(-200, 200)))
 
 
-def random_temp:
+main()
