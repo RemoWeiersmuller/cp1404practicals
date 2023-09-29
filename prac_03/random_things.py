a6f4388d3cdf7c1d8b5b7b70639c16fe4c347b42
Remo Weiersmuller <remo.weiersmueller@my.jcu.edu.au>
2023-09-29 10:21:08 +1000

    add random_things.py to repository

---
+++ prac_03/random_things.py
@@ -0,0 +1,13 @@
+"""Write 3 different versions of code to generate a random Boolean (True or False)."""
+
+import random
+
+
+def main():
+    random_boolean1 = random.choice([True, False])
+    print(random_boolean1)
+    random_boolean2 = bool(random.getrandbits(1))
+    print(random_boolean2)
+    random_boolean3 = bool(random.randint(0,1))
+    print(random_boolean3)
+main()
