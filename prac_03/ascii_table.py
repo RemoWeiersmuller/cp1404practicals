a74f8183261bd1fe5a3b14a14017c5f91187eb09
Remo Weiersmuller <remo.weiersmueller@my.jcu.edu.au>
2023-09-29 11:22:42 +1000

    add ascii_table.py to repository

---
+++ prac_03/ascii_table.py
@@ -0,0 +1,14 @@
+"""programm to get ASCII code or vis versa"""
+
+LOWER_NUMBER = 33
+UPPER_NUMBER = 127
+
+
+character = input("enter a character: ")
+print(ord(character))
+
+number = round(float(input(f"enter a number between {LOWER_NUMBER} and {UPPER_NUMBER}: ")))
+while number < LOWER_NUMBER or number > UPPER_NUMBER:
+    print("number is invalid")
+    number = round(float(input(f"enter a number between {LOWER_NUMBER} and {UPPER_NUMBER}: ")))
+print(chr(number))
