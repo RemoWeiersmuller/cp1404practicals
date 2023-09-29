4cf43d1bebfa198d60f1013a64acf4d95e38e1dc
Remo Weiersmuller <remo.weiersmueller@my.jcu.edu.au>
2023-09-29 11:55:07 +1000

    Add ASCII list to code

--- prac_03/ascii_table.py
+++ prac_03/ascii_table.py
@@ -1,10 +1,11 @@
-"""program to get ASCII code or vis versa"""
+"""Program to get ASCII code or vis versa."""
 
 LOWER_NUMBER = 33
 UPPER_NUMBER = 127
 
 
 def main():
+    """Program to get ASCII code or vis versa with a list."""
     character = input("enter a character: ")
     print(ord(character))
 
@@ -13,6 +14,9 @@ def main():
         print("number is invalid")
         number = round(float(input(f"enter a number between {LOWER_NUMBER} and {UPPER_NUMBER}: ")))
     print(chr(number))
+
+    for i in range(LOWER_NUMBER, UPPER_NUMBER):
+        print("{:>3}".format(i), chr(i))
 
 
 main()
