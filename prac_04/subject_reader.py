fa00e234ea4dbdf87e44360af0dc9c30b250e7b1
RemoWeiersmuller <remo.weiersmueller@my.jcu.edu.au>
2023-10-03 09:15:50 +0200

    Add display function

--- prac_04/subject_reader.py
+++ prac_04/subject_reader.py
@@ -4,11 +4,20 @@ Data file -> lists program
 """
 
 FILENAME = "subject_data.txt"
+subjects = []
 
 
 def main():
+    """Get data from txt file and display it."""
     data = get_data()
     print(data)
+    display_subject_details(data)
+
+
+def display_subject_details(data):
+    """Display details about subject."""
+    for details in data:
+        print(f"{details[0]} is taught by {details[1]} and has {details[2]} students.")
 
 
 def get_data():
@@ -22,8 +31,10 @@ def get_data():
         print(parts)  # See what the parts look like (notice the integer is a string)
         parts[2] = int(parts[2])  # Make the number an integer (ignore PyCharm's warning)
         print(parts)  # See if that worked
+        subjects.append(parts)
         print("----------")
     input_file.close()
+    return subjects
 
 
 main()
