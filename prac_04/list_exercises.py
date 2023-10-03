41a63febb7bb3095040bf5fb0e4eead59602bcb8
RemoWeiersmuller <remo.weiersmueller@my.jcu.edu.au>
2023-10-04 01:23:16 +0200

    Add display statements

--- prac_04/list_exercises.py
+++ prac_04/list_exercises.py
@@ -6,11 +6,9 @@ for i in range(number_of_numbers):
     number = int(input("Number: "))
     numbers.append(number)
 print(numbers)
+print(f"The first number is {numbers[0]}")
+print(f"The last number is {numbers[number_of_numbers-1]}")
+print(f"The smallest number is",min(numbers))
+print(f"The largest number is",max(numbers))
+print(f"The average of the numbers is",sum(numbers)/number_of_numbers)
 
-"""
-The first number is 5
-The last number is 3
-The smallest number is 1
-The largest number is 20
-The average of the numbers is 6.2
-"""
