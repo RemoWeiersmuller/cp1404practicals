67ace116bdcfb2374b9016f208a9e14d51686073
RemoWeiersmuller <remo.weiersmueller@my.jcu.edu.au>
2023-10-04 01:14:39 +0200

    Add list_exercises.py to repository

---
+++ prac_04/list_exercises.py
@@ -0,0 +1,16 @@
+number_of_numbers = 5
+numbers = []
+
+
+for i in range(number_of_numbers):
+    number = int(input("Number: "))
+    numbers.append(number)
+print(numbers)
+
+"""
+The first number is 5
+The last number is 3
+The smallest number is 1
+The largest number is 20
+The average of the numbers is 6.2
+"""
