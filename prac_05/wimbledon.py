85f8ebc1ebd2ee022f045588a16bd149252909ff
RemoWeiersmuller <remo.weiersmueller@my.jcu.edu.au>
2023-10-11 08:33:10 +0200

    add docstrings to wimbledon.py from prac05

--- prac_05/wimbledon.py
+++ prac_05/wimbledon.py
@@ -1,8 +1,10 @@
+"""Read records and display all champions and their country."""
 """
 Wimbledon
 Estimate: 30 minutes
 Actual:   45 minutes
 """
+
 
 FILENAME = 'wimbledon.csv'
 INDEX_COUNTRY = 1
@@ -10,12 +12,14 @@ INDEX_CHAMPION = 2
 
 
 def main():
+    """Read records and display all champions and their country."""
     records = read_records(FILENAME)
     display_champions(records)
     display_champions_country(records)
 
 
 def display_champions_country(records):
+    """Display the belonging countries of the champions."""
     countries = []
     for record in records:
         if not record[INDEX_COUNTRY] in countries:
@@ -25,6 +29,7 @@ def display_champions_country(records):
 
 
 def display_champions(records):
+    """Display the champions and the number of titles."""
     champion_to_count = {}
     for record in records:
         try:
@@ -37,6 +42,7 @@ def display_champions(records):
 
 
 def read_records(FILENAME):
+    """Read the record and return it as a list of lists."""
     records = []
     with open(FILENAME, "r", encoding="utf-8-sig") as in_file:
         in_file.readline()
