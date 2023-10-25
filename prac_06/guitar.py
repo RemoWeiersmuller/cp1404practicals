e7853667363fd4d72d0ddb68ada577d9106b3049
RemoWeiersmuller <remo.weiersmueller@my.jcu.edu.au>
2023-10-26 01:10:32 +0200

    Add guitar.py to prac_06

---
+++ prac_06/guitar.py
@@ -0,0 +1,17 @@
+class Guitar:
+    CURRENT_YEAR = 2023
+
+    def __init__(self, name="", year=0, cost=0):
+        self.name = name
+        self.year = year
+        self.cost = cost
+
+    def __str__(self):
+        return f"{self.name} ({self.year}) : ${sel.cost:,.2f}"
+
+    def get_age(self):
+        age = self.CURRENT_YEAR - self.year
+        return age
+
+    def is_vintage(self):
+        return self.get_age() >= 50
