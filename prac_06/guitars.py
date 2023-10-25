bf2c4570571ea7a1b7a7bd14447de21b643bc172
RemoWeiersmuller <remo.weiersmueller@my.jcu.edu.au>
2023-10-26 01:38:02 +0200

    Add guitars.py to prac_06

---
+++ prac_06/guitars.py
@@ -0,0 +1,37 @@
+"""
+languages
+Estimate: 30 minutes
+Actual:   20 minutes
+"""
+from prac_06.guitar import Guitar
+
+
+def main():
+    guitars = []
+    print("My guitars!")
+    # name = input("Name: ")
+    # year = input("Year: ")
+    # cost = float(input("Cost: "))
+    # guitars.append(Guitar(name, year, cost))
+    # print(f"{name} ({year}) : ${cost:.2f} added.")
+
+    guitars.append(Guitar("Gibson L-5 CES", 1922, 16035.40))
+    guitars.append(Guitar("Line 6 JTV-59", 2010, 1512.9))
+
+    for i, guitar in enumerate(guitars):
+        if Guitar.is_vintage(guitar):
+            vintage_string = "(vintage)"
+        else:
+            vintage_string = ""
+        # category = "adult" if age >= 18 else "child"
+        print(f"Guitar {i}: {guitar.name:>20} ({guitar.year}), worth ${guitar.cost:10,.2f}{vintage_string}")
+
+
+
+
+
+
+
+
+
+main()
