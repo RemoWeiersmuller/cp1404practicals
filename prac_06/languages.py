ead896c665881b2b0722d2a83c062e1319aad892
Remo Weiersmueller <remo.weiersmueller@my.jcu.edu.au>
2023-10-27 10:24:52 +1000

    Add doc string and main function in languages.py in prac_06

--- prac_06/languages.py
+++ prac_06/languages.py
@@ -1,18 +1,25 @@
 """
 languages
-Estimate: 30 minutes
-Actual:   35 minutes
+Estimate: 20 minutes
+Actual:   25 minutes
+Store a list of programming languages and give back th dynamic typed ones.
 """
 
 from programming_language import ProgrammingLanguage
 
-python = ProgrammingLanguage("Python", "Dynamic", True, 1991)
-ruby = ProgrammingLanguage("Ruby", "Dynamic", True, 1995)
-visual_basic = ProgrammingLanguage("Visual Basic", "Static", False, 1991)
-print(python)
 
-programminglanguages = [python, ruby, visual_basic]
-print("The dynamically typed languages are:")
-for language in programminglanguages:
-	if language.is_dynamic():
-		print(language.name)
+def main():
+    """Store a list of programming languages and give back th dynamic typed ones."""
+    python = ProgrammingLanguage("Python", "Dynamic", True, 1991)
+    ruby = ProgrammingLanguage("Ruby", "Dynamic", True, 1995)
+    visual_basic = ProgrammingLanguage("Visual Basic", "Static", False, 1991)
+    print(python)
+
+    programminglanguages = [python, ruby, visual_basic]
+    print("The dynamically typed languages are:")
+    for language in programminglanguages:
+        if language.is_dynamic():
+            print(language.name)
+
+
+main()
