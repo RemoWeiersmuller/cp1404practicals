680c06b39e84173a629f7b7eea155908eae39367
RemoWeiersmuller <remo.weiersmueller@my.jcu.edu.au>
2023-10-31 12:29:07 +0100

    Reformat programming_language.py in prac_07

--- prac_07/programming_language.py
+++ prac_07/programming_language.py
@@ -17,11 +17,13 @@ class ProgrammingLanguage:
 
     def __str__(self):
         """Return string representation of a ProgrammingLanguage."""
-        return f"{self.name}, {self.typing} Typing, Reflection={self.reflection}, First appeared in {self.year}, Pointer Arithmetic = {self.pointer_arithmetic}"
+        return (f"{self.name}, {self.typing} Typing, Reflection={self.reflection}, First appeared in {self.year}, "
+                f"Pointer Arithmetic = {self.pointer_arithmetic}")
 
     def __repr__(self):
         """Return string representation of a ProgrammingLanguage."""
-        return f"{self.name}, {self.typing} Typing, Reflection={self.reflection}, First appeared in {self.year}, Pointer Arithmetic = {self.pointer_arithmetic}"
+        return (f"{self.name}, {self.typing} Typing, Reflection={self.reflection}, First appeared in {self.year}, "
+                f"Pointer Arithmetic = {self.pointer_arithmetic}")
 
     def is_dynamic(self):
         """Determine if language is dynamically typed."""
