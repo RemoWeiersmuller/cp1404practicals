6275ea2c4b0ba59d995209b2dd4d7fcbfd7f1ad9
RemoWeiersmuller <remo.weiersmueller@my.jcu.edu.au>
2023-11-01 23:49:01 +0100

    Add docstring to myguitars.py in prac_07

--- prac_07/myguitars.py
+++ prac_07/myguitars.py
@@ -1,3 +1,5 @@
+"""Read, display guitars from a csv and adds new guitars from user input and saves it back to the csv."""
+
 import csv
 
 from prac_07.guitar import Guitar
@@ -6,21 +8,22 @@ FILENAME = "guitars.csv"
 
 
 def main():
+    """Read, display guitars from a csv and adds new guitars from user input and saves it back to the csv."""
     guitars = []
     records = read_csv()
-    for parts in records:
-        guitar = Guitar(parts[0], parts[1], float(parts[2]))
+    for record in records:
+        guitar = Guitar(record[0], record[1], float(record[2]))
         guitars.append(guitar)
-
     guitars.sort()
     for guitar in guitars:
         print(guitar)
 
+    add_new_guitar(guitars)
+    save_guitars(guitars)
+
 
 def read_csv():
     """Language file reader version using the csv module."""
-    # First, open the file for reading - note: specify newline
-    # to avoid quoted \n in strings being considered a new record
     in_file = open(FILENAME, 'r', newline='')
     reader = csv.reader(in_file)  # use default dialect, Excel
     lines = []
@@ -28,6 +31,26 @@ def read_csv():
         lines.append(row)
     in_file.close()
     return lines
+
+
+def add_new_guitar(guitars):
+    """Add new guitars from user input to the list."""
+    print("\nProvide details about your new guitar.")
+    new_guitar_name = input("Name: ")
+    while new_guitar_name != '':
+        new_guitar_year = int(input("Year: "))
+        new_guitar_price = float(input("Price: "))
+        guitar = Guitar(new_guitar_name, new_guitar_year, new_guitar_price)
+        guitars.append(guitar)
+        print("New guitar added to list")
+        new_guitar_name = input("Name: ")
+
+
+def save_guitars(guitars):
+    """Save all the guitars records to a CSV-file."""
+    with open(FILENAME, "w", encoding="utf-8-sig") as out_file:
+        for guitar in guitars:
+            print(f"{guitar.name},{guitar.year},{guitar.cost}", file=out_file)
 
 
 main()
