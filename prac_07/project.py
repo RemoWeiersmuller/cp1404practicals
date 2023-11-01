3bb8f5280905a83dd4ce68c8224f0c0a9cac8b8d
RemoWeiersmuller <remo.weiersmueller@my.jcu.edu.au>
2023-11-02 00:49:35 +0100

    Add project class to  prac_07

---
+++ prac_07/project.py
@@ -0,0 +1,20 @@
+"""Name	Start Date	Priority	Cost Estimate	Completion Percentage"""
+
+
+class Project:
+    def __init__(self, name="project", start_date="31/10/2022", priority=1, cost_estimate=0, completion_percentage=0):
+        self.name = name
+        self.start_date = start_date
+        self.priority = priority
+        self.cost_estimate = cost_estimate
+        self.completion_percentage = completion_percentage
+
+    def __str__(self):
+        return (f"{self.name}, start: {self.start_date}, priority {self.priority}, "
+                f"estimate: ${self.cost_estimate}, completion: {self.completion_percentage}%")
+
+    def __repr__(self):
+        return str(self)
+
+    def __lt__(self, other):
+        return self.priority < other.priority
