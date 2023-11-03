e497dfb1f21cf1d561e9ae623da5054503597c76
RemoWeiersmuller <remo.weiersmueller@my.jcu.edu.au>
2023-11-03 05:31:15 +0100

    Reformat project.py from prac_07

--- prac_07/project.py
+++ prac_07/project.py
@@ -1,6 +1,8 @@
 """Name	Start Date	Priority	Cost Estimate	Completion Percentage"""
 
 import datetime
+
+
 class Project:
     def __init__(self, name="project", start_date="31/10/2022", priority=1, cost_estimate=0, completion_percentage=0):
         self.name = name
