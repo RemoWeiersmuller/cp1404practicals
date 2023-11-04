6961d571c973f0a0613f270700b8fc30315fcd21
RemoWeiersmuller <remo.weiersmueller@my.jcu.edu.au>
2023-11-04 04:25:10 +0100

    Add docstrings to project.py from prac_07

--- prac_07/project.py
+++ prac_07/project.py
@@ -1,10 +1,16 @@
-"""Name	Start Date	Priority	Cost Estimate	Completion Percentage"""
-
-import datetime
+"""
+languages
+Estimate: 30 minutes
+Actual:   30 minutes
+Store details about a projects in a class.
+"""
 
 
 class Project:
+    """Store details about a projects in a class."""
+
     def __init__(self, name="project", start_date="31/10/2022", priority=1, cost_estimate=0, completion_percentage=0):
+        """Construct a project."""
         self.name = name
         self.start_date = start_date
         self.priority = priority
@@ -12,20 +18,26 @@ class Project:
         self.completion_percentage = completion_percentage
 
     def __str__(self):
+        """Return a string with details of projects."""
         return (f"{self.name}, start: {self.start_date.strftime('%d/%m/%Y')}, priority {self.priority}, "
                 f"estimate: ${self.cost_estimate:.2f}, completion: {self.completion_percentage}%")
 
     def __repr__(self):
+        """Return a string for a list given."""
         return str(self)
 
     def __lt__(self, other):
+        """Return if one project is less than another."""
         return self.priority < other.priority
 
     def __gt__(self, other):
+        """Return if one project is greater than another."""
         return self.priority > other.priority
 
     def __eq__(self, other):
+        """Return if one project is equal to another."""
         return self.priority == other.priority
 
     def is_incomplete(self):
+        """Return if a project is complete."""
         return int(self.completion_percentage) < 100
