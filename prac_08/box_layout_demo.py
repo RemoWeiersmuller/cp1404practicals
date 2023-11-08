262c998bcbe9df8836562bd5a4f36014b08a625f
RemoWeiersmuller <remo.weiersmueller@my.jcu.edu.au>
2023-11-08 04:01:25 +0100

    Add docstrings to box_layout_demo.py from prac_08

--- prac_08/box_layout_demo.py
+++ prac_08/box_layout_demo.py
@@ -1,17 +1,27 @@
+"""
+Estimate: 40 minutes
+Actual:   52 minutes
+Program with kivy GUI to enter a Name greet and clear the fields.."""
+
+
 from kivy.app import App
 from kivy.lang import Builder
 
 
 class BoxLayoutDemo(App):
+    """Build a class with the given view from a kv file."""
     def build(self):
+        """Build the GUI."""
         self.title = "Box Layout Demo"
         self.root = Builder.load_file('box_layout.kv')
         return self.root
 
     def handle_greet(self):
+        """Greet with given input in the label."""
         self.root.ids.output_label.text = f"Hello {self.root.ids.input_name.text}"
 
     def clear_fields(self):
+        """Clear the label and input field."""
         self.root.ids.input_name.text = ""
         self.root.ids.output_label.text = ""
 
