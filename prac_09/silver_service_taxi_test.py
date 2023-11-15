dc0454af97b1df01aace89899b15f6d454fcbe10
Remo Weiersmueller <remo.weiersmueller@my.jcu.edu.au>
2023-11-15 10:32:33 +1000

    Complete the silver_service_taxi_test.py from prac09

--- prac_09/silver_service_taxi_test.py
+++ prac_09/silver_service_taxi_test.py
@@ -1,1 +1,17 @@
-"""Silver servic taxi test"""
+"""Silver servic taxi class test"""
+from prac_09.silver_service_taxi import SilverServiceTaxi
+
+
+def main():
+    """Test the SilverServiceTaxi class."""
+    my_silver_service_taxi = SilverServiceTaxi(name='Hummer', fuel=200, fanciness=4)
+    print(my_silver_service_taxi)
+
+    limo_silver_service_taxi = SilverServiceTaxi(name='Limo', fuel=200, fanciness=2)
+    limo_silver_service_taxi.start_fare()
+    limo_silver_service_taxi.drive(18)
+    fare = limo_silver_service_taxi.get_fare()
+    print(fare)
+
+
+main()
