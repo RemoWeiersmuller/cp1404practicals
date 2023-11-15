494c9a832c91a9267743b072623fccbf4daa68ed
Remo Weiersmueller <remo.weiersmueller@my.jcu.edu.au>
2023-11-15 10:34:55 +1000

    Complete the silver_service_taxi.py from prac09

---
+++ prac_09/silver_service_taxi.py
@@ -0,0 +1,25 @@
+"""Silver service Taxi class
+Estimate: 60 minutes
+Actual:   30 minutes
+SilverServiceTaxi class, derived from Taxi
+"""
+from prac_09.taxi import Taxi
+
+
+class SilverServiceTaxi(Taxi):
+    """Class stores details about fancy taxis."""
+    flagfall = 4.50
+
+    def __init__(self, fanciness: float, **kwargs):
+        """Construct the Class"""
+        super().__init__(**kwargs)
+        self.fanciness = fanciness
+        self.price_per_km = self.price_per_km * self.fanciness
+
+    def __str__(self):
+        """Return a string for SilverServiceTaxi."""
+        return f"{super().__str__()} plus ${self.flagfall:.2f}"
+
+    def get_fare(self):
+        """Get the current fare."""
+        return super().get_fare() + self.flagfall
